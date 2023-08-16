6c05f8a2cde61f0c0a82735f23941a3b4e2464ba
Douglinhas <daguiaralcantara@gmail.com>
2023-08-16 16:34:44 -0300

    Implementação da classe Diretor

---
+++ src/Diretor.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Diretor extends Pessoa {
+
+    private ArrayList<Filme> filmes = new ArrayList<Filme>();
+
+    public Diretor(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setFilmes(Filme filme) {
+        this.filmes.add(filme);
+    }
+
+}
