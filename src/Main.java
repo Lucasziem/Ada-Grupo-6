9d75b6f9837935c29aa200f15df55664e9e7bd27
Amanda Puttomatti <acsputtomatti@gmail.com>
2023-08-21 20:03:24 -0300

    Adicionado atributos de coras para serem printadas no console

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_RED = "\u001B [31m";
+    public static final String ANSI_CIAN = "\u001B [36m";
     public static void cadastrarAtor(Scanner sc, IMDB imdb) {
         try {
             System.out.println("\n--- Cadastro de Ator ---");
@@ -15,9 +20,9 @@ public class Main {
             sc.nextLine();
             Ator ator = new Ator(nomeAtor, idadeAtor);
             imdb.adicionarAtor(ator);
-            System.out.println("Ator cadastrado!");
+            System.out.println(ANSI_GREEN + "Ator cadastrado!" + ANSI_RESET);
         } catch (InputMismatchException e) {
-            System.out.println("Precisa ser um número!");
+            System.out.println(ANSI_RED + "Precisa ser um número!" + ANSI_RESET);
             sc.nextLine();
             cadastrarAtor(sc, imdb);
         }
@@ -33,9 +38,9 @@ public class Main {
             sc.nextLine();
             Diretor diretor = new Diretor(nomeDiretor, idadeDiretor);
             imdb.adicionarDiretor(diretor);
-            System.out.println("Diretor cadastrado!");
+            System.out.println(ANSI_GREEN + "Diretor cadastrado!" + ANSI_RESET);
         } catch (InputMismatchException e) {
-            System.out.println("Precisa ser um número!");
+            System.out.println(ANSI_RED + "Precisa ser um número!" + ANSI_RESET);
             sc.nextLine();
             cadastrarDiretor(sc, imdb);
         }
@@ -56,19 +61,20 @@ public class Main {
             double orcamento = sc.nextDouble();
             Filme filme = new Filme(nomeFilme, descricaoFilme, date, orcamento);
             imdb.adicionarFilme(filme);
-            System.out.println("Filme cadastrado!");
+            System.out.println(ANSI_GREEN + "Filme cadastrado!" + ANSI_RESET);
         } catch (ParseException e) {
-            System.out.println("Formato de data errado. Tente novamente.");
+            System.out.println(ANSI_RED + "Formato de data errado. Tente novamente." + ANSI_RESET
+            );
             cadastrarFilme(sc, imdb);
         } catch (InputMismatchException e) {
-            System.out.println("Precisa ser um número!");
+            System.out.println(ANSI_RED + "Precisa ser um número!" + ANSI_RESET);
             sc.nextLine();
             cadastrarFilme(sc, imdb);
         }
     }
 
     public static boolean fluxoDoSistema(Scanner sc, IMDB imdb) {
-        System.out.println("\n----------------------------------");
+        System.out.println(ANSI_CIAN + "\n----------------------------------");
         System.out.println("1 - Cadastrar Ator");
         System.out.println("2 - Exibir todos os atores");
         System.out.println("3 - Cadastrar Diretor");
@@ -79,7 +85,7 @@ public class Main {
         System.out.println("8 - Exibir todos os filmes");
         System.out.println("9 - Pesquisar filme por nome");
         System.out.println("10 - Sair");
-        System.out.println("----------------------------------\n");
+        System.out.println("----------------------------------\n" + ANSI_RESET);
 
         System.out.println("Digite o numero da opção desejada: ");
 
@@ -94,7 +100,7 @@ public class Main {
                     return true;
                 }
                 case 2 -> {
-                    System.out.println("\n--- Lista de Atores ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Lista de Atores ---" + ANSI_RESET);
                     imdb.exibirTodosAtores();
                     return true;
                 }
@@ -103,7 +109,7 @@ public class Main {
                     return true;
                 }
                 case 4 -> {
-                    System.out.println("\n--- Lista de Diretores ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Lista de Diretores ---" + ANSI_RESET);
                     imdb.exibirTodosDiretores();
                     return true;
                 }
@@ -112,32 +118,32 @@ public class Main {
                     return true;
                 }
                 case 6 -> {
-                    System.out.println("\n--- Adicionar Diretor em um Filme ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Adicionar Diretor em um Filme ---" + ANSI_RESET);
                     System.out.println("Primeiro digite o nome do filme que deseja atualizar: ");
                     String nomeFilmeAtualizar = sc.nextLine();
                     System.out.println("Primeiro digite o nome do diretor que deseja adicionar: ");
                     String nomeDiretorAdd = sc.nextLine();
                     imdb.adicionarDiretorEmFilme(nomeFilmeAtualizar, nomeDiretorAdd);
-                    System.out.println("Filme atualizado!");
+                    System.out.println(ANSI_GREEN + "Filme atualizado!" + ANSI_RESET);
                     return true;
                 }
                 case 7 -> {
-                    System.out.println("\n--- Adicionar Ator em um Filme ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Adicionar Ator em um Filme ---" + ANSI_RESET);
                     System.out.println("Primeiro digite o nome do filme que deseja atualizar: ");
                     String nomeFilmeAtualizarAtor = sc.nextLine();
                     System.out.println("Primeiro digite o nome do ator que deseja adicionar: ");
                     String nomeAtorAdd = sc.nextLine();
                     imdb.adicionarAtorEmFilme(nomeFilmeAtualizarAtor, nomeAtorAdd);
-                    System.out.println("Filme atualizado");
+                    System.out.println(ANSI_GREEN + "Filme atualizado" + ANSI_RESET);
                     return true;
                 }
                 case 8 -> {
-                    System.out.println("\n--- Lista de Filmes ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Lista de Filmes ---" + ANSI_RESET);
                     imdb.exibirTodosFilmes();
                     return true;
                 }
                 case 9 -> {
-                    System.out.println("\n--- Pesquisa de Filme ---");
+                    System.out.println(ANSI_PURPLE + "\n--- Pesquisa de Filme ---" + ANSI_RESET);
                     System.out.println("Digite o nome do filme: ");
                     String nomeBusca = sc.next();
                     System.out.println(imdb.buscarFilmePorNome(nomeBusca));
@@ -152,7 +158,7 @@ public class Main {
                 }
             }
         } catch (InputMismatchException e) {
-            System.out.println("Precisa ser um número de 1 a 10.");
+            System.out.println(ANSI_RED + "Precisa ser um número de 1 a 10." + ANSI_RESET);
             return true;
         }
     }
